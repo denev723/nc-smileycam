51118c5bd4a637244d3dd373dfc66b346e6dc1d6
denev <denev723@gmail.com>
2021-08-10 06:13:36 +0900

    camera 기능 추가

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const CenterView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [hasPermission, setHasPermisstion] = useState(null);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const requestCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasPermisstion(status === "granted");
+  };
+
+  useEffect(() => {
+    requestCamera();
+  }, []);
+
+  if (hasPermission === null) {
+    return (
+      <CenterView>
+        <ActivityIndicator size="large" color="#000000" />
+      </CenterView>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <CenterView>
+        <Text>No access to camera</Text>
+      </CenterView>
+    );
+  } else {
+    return (
+      <CenterView>
+        <Camera
+          style={{
+            width: WIDTH - 20,
+            height: HEIGHT / 1.5,
+            borderRadius: 10,
+            overflow: "hidden",
+          }}
+          type={Camera.Constants.Type.front}
+        />
+      </CenterView>
+    );
+  }
+}
